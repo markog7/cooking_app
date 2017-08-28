613ee42b6ca54e57e349bb049147d457ce653e8f
markog7 <markgvozden@gmail.com>
2017-08-29 00:01:27 +0200

    added templates

--- ui/views.py
+++ ui/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('this is the index page')
+    context_built = {'data': 'I am data'}
+    return render(request, template_name='ui/index.html', context=context_built)
